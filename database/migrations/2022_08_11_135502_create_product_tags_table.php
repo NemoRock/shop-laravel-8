d13858bb0ea1e39a6f969569316d4e53237eb9e8
NemoRock <lexa.brus97@gmail.com>
2022-08-11 19:42:13 +0500

    added admin-products-CRUD step 1

---
+++ database/migrations/2022_08_11_135502_create_product_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_tags', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('tag_id')->nullable()->index()->constained('tags');
+            $table->foreignId('product_id')->nullable()->index()->constained('products');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_tags');
+    }
+}
